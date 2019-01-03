dba792c6c82d9e41429865ab67f3cf859a42648b
James Liu <jamesliu@au1.ibm.com>
2019-01-04 10:15:13 +1100

    feat: ignore blank lines when reading files

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -7,12 +7,20 @@ import (
 )
 
 // ReadFileIntoArray reads a .txt file, and returns an array of
-// strings representing each line of the file.
+// strings representing each non-empty line of the file.
 func ReadFileIntoArray(path string) ([]string, error) {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Println(err)
 	}
-	lines := strings.Split(string(content), "\n")
+
+	var lines []string
+
+	for _, line := range strings.Split(string(content), "\n") {
+		if line != "" {
+			lines = append(lines, line)
+		}
+	}
+
 	return lines, err
 }
