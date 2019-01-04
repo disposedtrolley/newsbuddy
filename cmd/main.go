89bbc988ebd63681c5cc03fe388230ab7fe62391
James Liu <jamesliu@au1.ibm.com>
2019-01-04 12:24:08 +1100

    feat: concurrently fetch titles for all articles

--- cmd/main.go
+++ cmd/main.go
@@ -26,36 +26,54 @@ func main() {
 
 	// Main processing loop
 	var articles []models.Article
+	ch := make(chan *models.Article, len(source.Articles))
+
 	for i, article := range source.Articles {
 		url := article.Url
 		log.Printf("[main] Processing article %d of %d\n", i+1, len(source.Articles))
-		title, err := summariser.ArticleTitle(url)
 
-		if err != nil {
-			log.Printf("[main] Error encountered while processing %s: %s\n", url, err.Error())
-		} else {
-			articles = append(articles, models.Article{URL: url, Title: title, Type: article.Type})
+		go func(url string, articleType string) {
+			title, err := summariser.ArticleTitle(url)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			ch <- &models.Article{URL: url, Title: title, Type: articleType}
+		}(url, article.Type)
+	}
+
+	// Iterate through the buffered channel and append processed articles
+	// to the articles slice.
+	for r := range ch {
+		articles = append(articles, *r)
+
+		// Once all articles have been processed, it's time to write to the
+		// template.
+		if len(articles) == len(source.Articles) {
+			fmt.Println("done!")
+			log.Printf("[main] Articles processed. Assembling data required for the template...")
+
+			data := models.NewsletterData{
+				Title:       source.Metadata.Title,
+				IssueNo:     source.Metadata.IssueNo,
+				PubDate:     source.Metadata.PubDate,
+				WelcomeText: source.Metadata.WelcomeText,
+				Articles:    articles}
+
+			outStr, err := formatter.FillTemplate(data)
+
+			if err != nil {
+				log.Fatalf("[main] An error occurred when generating the template: %s\n", err.Error())
+			}
+
+			log.Printf("[main] Writing output to file...")
+
+			w := writer.NewFileWriter(fmt.Sprintf("%s.mjml", filePath))
+
+			w.WriteToFile(outStr)
+
+			return
 		}
 	}
-
-	log.Printf("[main] Articles processed. Assembling data required for the template...")
-
-	data := models.NewsletterData{
-		Title:       source.Metadata.Title,
-		IssueNo:     source.Metadata.IssueNo,
-		PubDate:     source.Metadata.PubDate,
-		WelcomeText: source.Metadata.WelcomeText,
-		Articles:    articles}
-
-	outStr, err := formatter.FillTemplate(data)
-
-	if err != nil {
-		log.Fatalf("[main] An error occurred when generating the template: %s\n", err.Error())
-	}
-
-	log.Printf("[main] Writing output to file...")
-
-	w := writer.NewFileWriter(fmt.Sprintf("%s.mjml", filePath))
-
-	w.WriteToFile(outStr)
 }
